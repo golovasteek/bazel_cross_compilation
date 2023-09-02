522a94ffce978196c1f6b3c1154738545b19bb08
Evgeny Petrov <evgeny@epetrov.net>
2023-09-02 23:55:53 +0200

    Vendor in toolchain for x86_64-linux-musl target

---
+++ toolchains/thirdparty/x86_64-unknown-linux-musl/lib/gcc/x86_64-unknown-linux-musl/11.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/build/.build/x86_64-unknown-linux-musl/src/gcc/configure --build=x86_64-build_apple-darwin19.6.0 --host=x86_64-build_apple-darwin19.6.0 --target=x86_64-unknown-linux-musl --prefix=/Volumes/tools/x86_64-unknown-linux-musl --exec_prefix=/Volumes/tools/x86_64-unknown-linux-musl --with-sysroot=/Volumes/tools/x86_64-unknown-linux-musl/x86_64-unknown-linux-musl/sysroot --enable-languages=c,c++,fortran --enable-__cxa_atexit --disable-libmudflap --enable-libgomp --enable-libssp --enable-libquadmath --enable-libquadmath-support --disable-libsanitizer --disable-libmpx --disable-libstdcxx-verbose --with-gmp=/Volumes/build/.build/x86_64-unknown-linux-musl/buildtools --with-mpfr=/Volumes/build/.build/x86_64-unknown-linux-musl/buildtools --with-mpc=/Volumes/build/.build/x86_64-unknown-linux-musl/buildtools --with-isl=/Volumes/build/.build/x86_64-unknown-linux-musl/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --with-linker-hash-style=both --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/Volumes/tools/x86_64-unknown-linux-musl/x86_64-unknown-linux-musl/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
